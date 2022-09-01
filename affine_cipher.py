9c1d1ab3be8ca8f5148574c781bfc6c6d9454971
Yujian Tang <yujian@Yujians-MacBook-Pro.local>
2022-08-31 17:07:21 -0700

    upgrading affine cipher

--- affine_cipher.py
+++ affine_cipher.py
@@ -6,7 +6,7 @@ def encode():
     print(encoded)
     return encoded
 
-encode()
+# encode()
 
 def decode():
     encoded_list = input("What are is the encoded affine cipher? ")
@@ -14,4 +14,47 @@ def decode():
     print(decoded)
     return decoded
 
-decode()
+# decode()
+
+# take the distance from the letter 'a' as the intercept
+# allow an entry, m, for the scaling
+def custom_encode(word, m = 1, b = 0, base = 'a', return_string = False):
+    encoded = []
+    for char in word:
+        x = ord(char) - ord(base)
+        y = m * x + b
+        encoded.append(y)
+    if return_string:
+        return ' '.join(encoded)
+    else:
+        return encoded
+
+# x = (y - b)/m
+def custom_decode(encoded, m = 1, b = 0, base = 'a', return_string = True):
+    decoded = []
+    for y in encoded:
+        x = (y-b)//m
+        x = chr(x + ord(base))
+        decoded.append(x)
+    if return_string:
+        return ''.join(decoded)
+    else:
+        return decoded
+
+# basic 
+cipher = custom_encode("ego")
+print(f"Encoded affine cipher is: {cipher}")
+word = custom_decode(cipher)
+print(f"Custom decoded affine cipher word is: {word}")
+
+# customized
+cipher = custom_encode("ego", m=2, b=3)
+print(f"Encoded affine cipher is: {cipher}")
+word = custom_decode(cipher, m=2, b=3)
+print(f"Custom decoded affine cipher word is: {word}")
+
+# incorrect
+cipher = custom_encode("ego", m=2, b=3)
+print(f"Encoded affine cipher is: {cipher}")
+word = custom_decode(cipher, m=3, b=1)
+print(f"Custom decoded affine cipher word is: {word}")
